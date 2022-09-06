357114a33d75e85bf40c68a84ab77685df84a123
aaron sharpe <aaron.sharpe@hingehealth.com>
2022-09-06 14:51:15 -0700

    i can push right to main on my own repo

---
+++ script.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -euo pipefail
+trap "echo YAAAAAAAAAAAAAAAAAAAAY WE GOT THE SIGNAL; exit" SIGINT SIGTERM EXIT SIGHUP SIGKILL
+
+while true
+do
+    echo "${SWEAR_WORD}"
+    sleep 1
+done
